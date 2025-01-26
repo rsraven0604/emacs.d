0045b700de01c95271f7f95c6f0d330064f927d1
SHIMA Ryosuke <shima@unalus.com>
2025-01-26 20:45:38 +0900

    init.elを追加

---
+++ init.el
@@ -0,0 +1,161 @@
+
+;; 余計なファイルを生成しない
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq auto-save-list-file-prefix nil)
+
+
+;; C-x C-c で閉じる時に、ワンクッション置く
+(setq confirm-kill-emacs 'y-or-n-p)
+
+;; パッケージリポジトリの設定
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(package-initialize)
+
+;; use-packageのインストール
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+;; いらないUIを消す
+(setq inhibit-startup-message t)       ;; スタートアップメッセージを非表示
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+
+(global-display-line-numbers-mode t)   ;; 行番号を表示
+
+;; インデント設定
+(setq-default indent-tabs-mode nil)  ;; タブをスペースに変換
+(setq-default tab-width 4)           ;; タブ幅を4に設定
+(setq indent-line-function 'insert-tab)
+
+;; フォント設定
+(set-face-attribute 'default nil :family "Monaspace Xenon Var" :height 140)
+
+;; ウィンドウサイズ指定
+(set-frame-size (selected-frame) 120 48)
+
+;; キーバインド
+;;; 削除ボタンを C-hに設定
+(global-set-key "\C-h" `delete-backward-char)
+
+;;; アクティブウィンドウ切り替えを C-tに設定
+(global-set-key "\C-t" `other-window)
+
+;; シンタックスハイライト
+(global-font-lock-mode t)
+
+;; 行末の空白を可視化
+(setq show-trailing-whitespace t)
+
+
+;; テーマの設定
+(use-package leuven-theme
+  :ensure t
+  :load-path "themes"
+  :init
+  :config
+  (load-theme 'leuven-dark t)
+  )
+
+;; 背景透過
+(set-frame-parameter nil 'alpha-background 70)
+(add-to-list 'default-frame-alist '(alpha-background . 60))
+
+;; org-mode, org-agendaの設定
+;; 参考 https://qiita.com/mamo3gr/items/6324b695131fef9b6031
+(use-package org
+  :commands org-agenda
+  :mode (("\\.org\\'" . org-mode))
+  :init
+  (setq org-directory "~/Dropbox/agenda/")
+  (setq org-default-notes-file (concat org-directory "main.org"))
+  (setq org-agenda-files (list org-directory))
+  (defun my:org-goto-inbox ()
+    (interactive)
+    (find-file org-default-notes-file))
+  :config
+  (setq system-time-locale "C")  ;; to avoid Japanese in the time stamp
+  (setq org-hide-leading-stars t)
+  (setq org-startup-folded nil)
+  ;; org-capture and enrty
+  (setq org-todo-keywords
+        '((sequence "TASK(t)" "WAIT(w)" "|" "DONE(d)" "ABORT(a)" "SOMEDAY(s)")))
+  (setq org-tag-alist '(("PROJECT" . ?p) ("MEMO" . ?m) ("PETIT" . ?t)))
+  (setq org-capture-templates
+        '(("t" "Task" entry (file+headline org-default-notes-file "inbox")
+           "** TASK %?\n   CREATED: %U\n")
+          ("i" "Idea" entry (file+headline org-default-notes-file "idea")
+           "** %?\n   CREATED: %U\n")
+          ("k" "Knowledge" entry (file+headline org-default-notes-file "knowledge")
+           "** %?\n   CREATED: %U\n")))
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 1)))
+  ;; org-clock
+  (setq org-log-done 'time)  ;; add a time stamp to the task when done
+  (setq org-clock-clocked-in-display 'frame-title)
+  :bind
+  (("C-c a" . org-agenda)
+   ("C-c c" . org-capture)
+   ("C-c g" . org-clock-goto)
+   ("C-c i" . my:org-goto-inbox)
+   :map org-mode-map
+   ("C-m" . org-return-indent)
+   ("M-n" . org-forward-same-level)
+   ("M-p" . org-backward-same-level)))
+
+(use-package org-agenda
+  :commands org-agenda
+  :config
+  (setq org-agenda-custom-commands
+        '(("x" "Unscheduled Tasks" tags-todo
+           "-SCHEDULED>=\"<today>\"-DEADLINE>=\"<today>\"" nil)
+          ("d" "Daily Tasks" agenda ""
+           ((org-agenda-span 1)))))
+  (setq org-agenda-skip-scheduled-if-done t)
+  (setq org-return-follows-link t)  ;; RET to follow link
+  (setq org-agenda-columns-add-appointments-to-effort-sum t)
+  (setq org-agenda-time-grid
+        '((daily today require-timed)
+          (0900 1200 1300 2000) "......" "----------------"))
+  (setq org-columns-default-format
+        "%68ITEM(Task) %6Effort(Effort){:} %6CLOCKSUM(Clock){:}")
+  (defadvice org-agenda-switch-to (after org-agenda-close)
+    "Close a org-agenda window when RET is hit on the window."
+    (progn (delete-other-windows)
+           (recenter-top-bottom)))
+  (ad-activate 'org-agenda-switch-to)
+  :bind
+  (:map org-agenda-mode-map
+        ("s" . org-agenda-schedule)
+        ("S" . org-save-all-org-buffers)))
+
+
+;; ob-mermaid
+(use-package ob-mermaid
+  :ensure t
+  :config
+  (org-babel-do-load-languages
+    'org-babel-load-languages
+    '((mermaid . t)
+      (scheme . t)))
+  (setq ob-mermaid-cli-path "/usr/bin/mmdc"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(leuven-theme kaolin-themes cyberpunk-theme ob-mermaid monokai-theme company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
